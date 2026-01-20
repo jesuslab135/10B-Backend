8e6ab3c5d2d533a9f96366098047b602c021f482
jesuslab135 <jefeing.esteban@gmail.com>
2026-01-20 14:24:43 -0800

    20/01/26 Swagger with Sections' Methods

--- Backend10B/api/urls.py
+++ Backend10B/api/urls.py
@@ -6,13 +6,13 @@ from api import views
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
 
-router.register(r"groups", views.GroupViewSet, basename="group")
-router.register(r"users", views.UserViewSet, basename="user")
-router.register(r"profiles", views.ProfileViewSet, basename="profile")
-router.register(r"blitzes", views.BlitzViewSet, basename="blitz")
-router.register(r"matches", views.MatchViewSet, basename="match")
-router.register(r"chats", views.ChatViewSet, basename="chat")
-router.register(r"messages", views.MessageViewSet, basename="message")
+router.register(r'groups', views.GroupViewSet, basename="group")
+router.register(r'users', views.UserViewSet, basename="user")
+router.register(r'profiles', views.ProfileViewSet, basename="profile")
+router.register(r'blitzes', views.BlitzViewSet, basename="blitz")
+router.register(r'matches', views.MatchViewSet, basename="match")
+router.register(r'chats', views.ChatViewSet, basename="chat")
+router.register(r'messages', views.MessageViewSet, basename="message")
 
 urlpatterns = [
     path("", include(router.urls)),
--- Backend10B/api/views.py
+++ Backend10B/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from drf_spectacular.utils import extend_schema
 from api.models import Group, User, Profile, Blitz, Match, Chat, Message
 from api.serializers import GroupSerializer, UserSerializer, ProfileSerializer, BlitzSerializer, MatchSerializer, ChatSerializer, MessageSerializer
 
@@ -8,36 +9,43 @@ from rest_framework import generics
 # Create your views here.
 
 # Group API ViewSet
+@extend_schema(tags=['Groups'])
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
 # User API ViewSet
+@extend_schema(tags=['Users'])
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 # Profile API ViewSet
+@extend_schema(tags=['Profiles'])
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
 # Blitz API ViewSet
+@extend_schema(tags=['Blitzes'])
 class BlitzViewSet(viewsets.ModelViewSet):
     queryset = Blitz.objects.all()
     serializer_class = BlitzSerializer
 
 # Match API ViewSet
+@extend_schema(tags=['Matches'])
 class MatchViewSet(viewsets.ModelViewSet):
     queryset = Match.objects.all()
     serializer_class = MatchSerializer
 
 # Chat API ViewSet
+@extend_schema(tags=['Chats'])
 class ChatViewSet(viewsets.ModelViewSet):
     queryset = Chat.objects.all()
     serializer_class = ChatSerializer
 
 # Message API ViewSet
+@extend_schema(tags=['Messages'])
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
