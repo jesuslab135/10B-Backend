fa8fbc928fa9f55a0cef801cbd34efd117457d7a
jesuslab135 <jefeing.esteban@gmail.com>
2026-01-21 19:10:01 -0800

    21/01/26 HyperLinkedSerializer

--- Backend10B/api/serializers.py
+++ Backend10B/api/serializers.py
@@ -1,37 +1,37 @@
 from rest_framework import serializers
 from api.models import User, Group, Profile, Blitz, Chat, Match, Message
 
-class GroupSerializer(serializers.ModelSerializer):
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = '__all__'
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
 
-class ProfileSerializer(serializers.ModelSerializer):
+class ProfileSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Profile
         fields = '__all__'
 
-class BlitzSerializer(serializers.ModelSerializer):
+class BlitzSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Blitz
         fields = '__all__'
 
-class MatchSerializer(serializers.ModelSerializer):
+class MatchSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Match
         fields = '__all__'
 
-class ChatSerializer(serializers.ModelSerializer):
+class ChatSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Chat
         fields = '__all__'
 
-class MessageSerializer(serializers.ModelSerializer):
+class MessageSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Message
         fields = '__all__'
